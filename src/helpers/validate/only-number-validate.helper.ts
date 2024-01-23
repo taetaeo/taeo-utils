d612b6abe75e90c858b141a5b438e8967ef5189e
ohtaekwon <ohtk1119@gmail.com>
2024-01-24 01:33:04 +0900

    [ADD] 오직 숫자형태만 출력이 되는 기능을 가진 함수 추가

---
+++ src/helpers/validate/only-number-validate.helper.ts
@@ -0,0 +1,13 @@
+/**
+ * 숫자형식을 포함한 input의 문자열 중 오직 숫자형만 출력이 되도록하는 함수
+ * @param {string} input ex) '12ads3'
+ * @returns {string} '123'
+ */
+
+export function onlyNumberValidateHelper(input: string) {
+  const result = input.replace(/[^0-9]/g, "");
+
+  console.assert(result, `결과: onlyNumberValidate에서 ${input}이 입력되었고, 빈문자열이 반환되었습니다.`);
+
+  return result;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+// Checker
 export { isAlpahbetCheckerHelper } from "./helpers/checker/is-alpahbet-checker.helper";
 export { equalValueCheckerHelper } from "./helpers/checker/equal-value-checker.helper";
+// Date
 export { datePlusYearHelper } from "./helpers/date/date-plus-year.helper";
+//Form
+export { phoneNumberToArrayHelper } from "./helpers/form/phone-number-to-array.helper";
+// Validate
+export { onlyNumberValidateHelper } from "./helpers/validate/only-number-validate.helper";

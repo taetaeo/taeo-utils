96613e5db5998ee83cc873ca72a84c07abe08bf7
ohtaekwon <ohtk1119@gmail.com>
2024-01-23 11:15:46 +0900

    [FIX] 오타로 인한 버그 수정

--- index.cjs
+++ index.cjs
@@ -1,5 +1,5 @@
 const equalValueCheckerHelper = require("./helpers/checker/equal-value-checker.helper");
-const isAlphabetCheckerHelper = require("./helpers/checker/is-alphabet-checker.helper");
+const isAlphabetCheckerHelper = require("./helpers/checker/is-alpahbet-checker.helper");
 const datePlusYearHelper = require("./helpers/date/date-plus-year.helper");
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
+export * from "./helpers/checker/is-alpahbet-checker.helper";
 export * from "./helpers/checker/equal-value-checker.helper";
-export * from "./helpers/checker/is-alphabet-checker.helper";
 export * from "./helpers/date/date-plus-year.helper";

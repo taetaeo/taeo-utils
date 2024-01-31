d2905938483e6df255f8b46a77b968909481c627
ohtaekwon <ohtk1119@gmail.com>
2024-01-31 14:19:46 +0900

    [UPDATE] 로컬스토리지 모듈의 읽기, 쓰기, 삭제 기능 추가

--- src/modules/local-storage.module.ts
+++ src/modules/local-storage.module.ts
@@ -1,32 +1,53 @@
+const isServer = typeof window === "undefined";
+
 function get<T>(key: string): T | null {
+  if (isServer) {
+    console.warn("현재 브라우저 환경이 아닙니다.");
+    return null;
+  }
+
   try {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : null;
   } catch (error) {
-    console.error(`Error while getting data from localStorage for key '${key}':`, error);
+    console.error(`실패 : 스토리지의 데이터를 불러오는데 실패하였습니다. '${key}':`, error);
     return null;
   }
 }
 
-function set<T>(key: string): T | null {
+function set<T>(key: string, value: T): void | null {
+  if (isServer) {
+    console.warn("현재 브라우저 환경이 아닙니다.");
+    return null;
+  }
+
+  if (typeof key === "object") {
+    return null;
+  }
   try {
-    if (typeof key === "object") {
-    }
-
-    return null;
+    const serializedValue = JSON.stringify(value);
+    localStorage.setItem(key, serializedValue);
   } catch (error) {
+    console.warn("실패 : 스토리지에 데이터를 저장하는데 실패하였습니다.");
     return null;
   }
 }
 
-function update() {}
-
-function remove() {}
+function remove(key: string): void | null {
+  if (isServer) {
+    console.warn("현재 브라우저 환경이 아닙니다.");
+    return null;
+  }
+  try {
+    return window.localStorage.removeItem(key);
+  } catch (error) {
+    console.warn(`실패 : 스토리지에서 항목을 삭제하는데 실패하였습니다. 키 : ${key} `);
+  }
+}
 
 const localStorageModule = {
   get: get,
   set: set,
-  update: update,
   remove: remove,
 };
 export default localStorageModule;
